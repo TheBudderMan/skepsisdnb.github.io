5afa7cd4f96111d74262c09e1a46a44b401adfa5
TheBudderMan <skepsisdrumandbass@gmail.com>
2024-02-06 23:59:23 -0600

    Hamburger Menu for desktop and mobile changed

--- navigation-functions.js
+++ navigation-functions.js
@@ -1,25 +1,25 @@
 //Hamburger Navigation Funtions
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-    document.getElementById("hamburgerButton1").style.visibility='hidden';
+  document.getElementById("myNav").style.width = "100%";
+  document.getElementById("hamburgerButton1").style.visibility='hidden';
+}
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+  document.getElementById("hamburgerButton1").style.visibility='visible';
+}
+//
+var overlayPage = document.getElementById('hamburgerButton1');
+function showDiv() {
+  overlayPage.style.display = "block";
+}
+
+function closeDiv() {
+  overlayPage.style.display = "none";
+}
+function showOrHide() {
+  if (overlayPage.style.display === "block") {
+    closeDiv()
+  } else {
+    showDiv()
   }
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-    document.getElementById("hamburgerButton1").style.visibility='visible';
-  }
-  //
-  var overlayPage = document.getElementById('hamburgerButton1');
-  function showDiv() {
-    overlayPage.style.display = "block";
-  }
-  
-  function closeDiv() {
-    overlayPage.style.display = "none";
-  }
-  function showOrHide() {
-    if (overlayPage.style.display === "block") {
-      closeDiv()
-    } else {
-      showDiv()
-    }
-  }
+}
