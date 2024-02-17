fc6e9be9ad94887bce1237096af3bc1b2e390efb
TheBudderMan <skepsisdrumandbass@gmail.com>
2024-02-17 15:53:53 -0600

    did not need that lol

---
+++ swipeBox.js
@@ -0,0 +1,16 @@
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 6,
+    spaceBetween: 10,
+    slidesPerGroup: 2,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
