d6ffb56fa6543a216dc533fc990d64563939a1c8
TheBudderMan <skepsisdrumandbass@gmail.com>
2025-01-20 21:32:59 -0600

    well gotta add some meta data.

---
+++ CatGameDevLog.cs
@@ -0,0 +1,31 @@
+//
+
+/*
+Idk why, but writing in a C# script makes me feel better. Some of that hipster love I spose.
+
+So with this cat game, we gotta start really focusing if we want to have something made before summer.
+
+We need to trim down on scope and scale for the demo. 
+
+Level and terrain will be pretty much done, with implied ideas for expansion into a city or other neighbourhood.
+
+as we start to animate more, we also have to focus on implementing more level mechanics in the design of the walls and rest of the basement. 
+
+That's probably the number one, finishing off the furniture and what you would be adding to the game in terms of what can be climbed on.
+
+The main selling point is the cartoon aspect of the parkour kitty. All kitties can do parkour, they're fucking cats.
+
+We have to start associating cat facts with the player mechanics. I wasn't okay with the cartoon fight animations at first, but it's actually okay.
+
+so big thing today is, we can either figure out the animations for the humans and do more cinemachine and scene direction. 
+
+we can work on enemy interactions and collision. 
+
+If we're to do more cut scene animation we have to start implementing correct camera techniques, film people are easy to upset lol.
+
+So I guess, what you actually need to do, is strip down the basement to only the basement and separate that into it's own scene.
+
+I guess before that, we should actually separate the assets, like the walls and rooms. 
+
+
+*/
