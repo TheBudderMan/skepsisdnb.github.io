8037e2293adc0c6d67b74200ce37b00d69ff1c5d
TheBudderMan <skepsisdrumandbass@gmail.com>
2025-05-13 10:44:49 -0500

    ads.txt file updated

--- formantReactServer/vite.config.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="vitest" />
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  
-});
